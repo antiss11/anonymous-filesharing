673f2e019630a07b517e55618ef9122eaed898da
Dmitry Kulik <recc1337@gmail.com>
2022-10-19 13:16:28 +0300

    Add styles to File component, add url field to interface

--- frontend/src/Components/File.tsx
+++ frontend/src/Components/File.tsx
@@ -1,20 +1,32 @@
-import {IFile} from "./LastUploadedFiles";
+import React from 'react';
 
-function File({name, uploadedDate, size}: IFile) {
+export interface IFile {
+  name: string;
+  uploadedDate: string;
+  size: string;
+  url: string;
+}
+
+function File({
+  name, uploadedDate, size, url,
+}: IFile) {
   return (
-    <div className="bg-white p-3 rounded-xl">
-      <a className="underline italic text-blue-600 cursor-pointer hover:text-blue-800">
+    <div className="bg-white p-3 my-3 rounded-xl">
+      <a href={url} className="underline italic text-blue-600 cursor-pointer hover:text-blue-800">
         {name}
       </a>
-      <div className="flex justify-between">
-        <p>Uploaded: 
+      <div className="flex justify-between flex-col sm:flex-row">
+        <p>
+          Uploaded:
           <span className="px-1">{uploadedDate}</span>
         </p>
-        <p>Size:
+        <p>
+          Size:
           <i className="px-1">{size}</i>
         </p>
       </div>
-  </div>)
+    </div>
+  );
 }
 
 export default File;
