904cf19a540925ac72e8bc83bec456f040395349
Dmitry Kulik <recc1337@gmail.com>
2022-10-20 15:15:01 +0300

    Insert Dropzone into UploadForm

--- frontend/src/Components/UploadForm.tsx
+++ frontend/src/Components/UploadForm.tsx
@@ -1,4 +1,6 @@
 import React, { FormEvent } from 'react';
+import { FileWithPath } from 'react-dropzone';
+import Dropzone, { IFile } from './Dropzone';
 
 export type SubmitEvent = FormEvent<HTMLFormElement>;
 
@@ -6,27 +8,43 @@ export type HandleSubmit = {
   (e: SubmitEvent): void;
 };
 
-type Props = {
-  handleSubmit: HandleSubmit;
+export type HandleDrop = {
+  (file: FileWithPath): void;
 };
 
-export function UploadForm({ handleSubmit }: Props) {
+type Props = {
+  handleSubmit: HandleSubmit;
+  handleDrop: HandleDrop;
+  file: IFile;
+};
+
+export function UploadForm({ handleSubmit, handleDrop, file }: Props) {
   return (
-    <form onSubmit={handleSubmit} className="py-5 flex items-center flex-col sm:flex-col justify-center md:w-1/2 bg-yellow-200">
-      <div className="bg-orange-200 p-4 rounded-xl">
-        <div className="flex flex-col items-center">
-          <input type="file" name="file" className="" />
-          <label htmlFor="is-anon" className="align-middle flex items-center my-3">
-            <input type="checkbox" name="isAnon" id="is-anon" className="mx-3 w-5 h-5" />
-            Anonymous
-          </label>
-        </div>
-        <input
-          type="submit"
-          value="Upload"
-          className="cursor-pointer bg-red-300 hover:bg-red-500 px-10 py-2 rounded-2xl w-auto"
-        />
-      </div>
+    <form
+      onSubmit={handleSubmit}
+      className="py-5 flex items-center flex-col sm:flex-col justify-center w-4/5"
+    >
+      <Dropzone handleDrop={handleDrop} file={file} />
+      {file && (
+        <>
+          <div className="my-4">
+            <label htmlFor="is-anon" className="flex">
+              <input
+                type="checkbox"
+                name="isAnon"
+                id="is-anon"
+                className="mx-3 w-5 h-5"
+              />
+              Anonymous
+            </label>
+          </div>
+          <input
+            type="submit"
+            value="Upload"
+            className="cursor-pointer bg-red-300 hover:bg-red-500 px-10 py-2 rounded-2xl w-auto"
+          />
+        </>
+      )}
     </form>
   );
 }
