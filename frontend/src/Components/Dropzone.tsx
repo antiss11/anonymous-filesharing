aab89989df79965319c50a08c5f28727bf7b19cc
Dmitry Kulik <recc1337@gmail.com>
2022-10-21 16:07:37 +0300

    Add error handling and animation to Dropzone, refactoring

--- frontend/src/Components/Dropzone.tsx
+++ frontend/src/Components/Dropzone.tsx
@@ -1,5 +1,12 @@
-import React, { useCallback } from 'react';
-import { FileWithPath, useDropzone } from 'react-dropzone';
+import React, { useCallback, useState, useRef } from 'react';
+import {
+  FileError,
+  FileRejection,
+  FileWithPath,
+  useDropzone,
+} from 'react-dropzone';
+import anime, { AnimeAnimParams, AnimeTimelineInstance } from 'animejs';
+import clsx from 'clsx';
 
 export type IFile = FileWithPath | null;
 
@@ -8,34 +15,113 @@ interface Props {
   file: IFile;
 }
 
+function addActionsToTimeline<T extends AnimeTimelineInstance>(
+  timeline: T,
+  actions: Array<AnimeAnimParams>,
+): T;
+function addActionsToTimeline<T extends AnimeTimelineInstance>(
+  timeline: T,
+  actions: Array<AnimeAnimParams>,
+  loop: number,
+  lastAction?: AnimeAnimParams,
+): T;
+function addActionsToTimeline(
+  timeline: AnimeTimelineInstance,
+  actions: Array<AnimeAnimParams>,
+  loop?: number,
+  lastAction?: AnimeAnimParams,
+) {
+  const addActions = () => {
+    actions.forEach((action) => {
+      timeline.add(action);
+    });
+  };
+
+  const addActionsInLoop = (n: number) => {
+    for (let i = 1; i <= n; i += 1) {
+      addActions();
+    }
+  };
+
+  if (loop) {
+    addActionsInLoop(loop);
+  } else {
+    addActions();
+  }
+  if (lastAction) timeline.add(lastAction);
+}
+
 export default function Dropzone({ handleDrop, file }: Props) {
-  const onDrop = useCallback((files: Array<File>) => {
-    // Array with single File
-    handleDrop(files[0]);
-  }, []);
+  const [error, setError] = useState<FileError | null>(null);
+
+  const animationElementRef = useRef(null);
+
+  const onErrorAnimation = () => {
+    const target = animationElementRef.current;
+    const timeline = anime.timeline({
+      easing: 'easeInElastic',
+      targets: target,
+    });
+
+    addActionsToTimeline(
+      timeline,
+      [
+        { rotateZ: '3deg', duration: 10 },
+        { rotateZ: '-3deg', duration: 10 },
+      ],
+      10,
+      { rotateZ: 0, duration: 20 },
+    );
+  };
+
+  const onDrop = useCallback(
+    (files: Array<FileWithPath>, errors: Array<FileRejection>) => {
+      // Array with single File
+      if (errors.length > 0) {
+        const errorObj = errors[0].errors[0];
+        setError(errorObj);
+        onErrorAnimation();
+      } else {
+        handleDrop(files[0]);
+      }
+    },
+    [],
+  );
 
   const { getRootProps, getInputProps } = useDropzone({
     maxFiles: 1,
     onDrop,
   });
 
-  const classNames = [
-    'px3',
-    'word-wrap',
-    file && 'text-black/100',
-    file && 'font-bold',
-    file || 'text-black-40',
-  ].join(' ');
-
   return (
-    <section className="container w-5/6 bg-white outline-dashed outline-2 outline-gray-400">
+    <section
+      className={clsx(
+        'container',
+        'w-5/6',
+        'bg-white',
+        'outline-dashed',
+        error ? 'outline-4' : 'outline-2',
+        error ? 'outline-red-500' : 'outline-gray-400',
+      )}
+      ref={animationElementRef}
+    >
       <div {...getRootProps({ className: 'dropzone py-16 cursor-pointer' })}>
         <input {...getInputProps()} />
-        <p
-          className={classNames}
-        >
-          {file ? file.path : 'Drop file here, or click to select'}
-        </p>
+        <div>
+          <p
+            className={clsx(
+              'px-3',
+              'word-wrap',
+              file ? 'text-black/100' : 'text-black-40',
+              file ? 'font-bold' : 'font-normal',
+            )}
+          >
+            {file ? file.path : 'Drop file here, or click to select'}
+          </p>
+          <p className="text-xl font-bold text-red-600">
+            {error ? error.message : ''}
+          </p>
+        </div>
       </div>
     </section>
   );
